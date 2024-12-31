67c1a0a5a8f70e0ff9157571f8d50d4c8b0149b1
Vijo Cherian <codervijo@gmail.com>
2024-12-30 22:12:47 -0800

    Ratatui-based app to view hackernews

---
+++ src/hnreader.rs
@@ -0,0 +1,57 @@
+use reqwest::Error;
+use serde::Deserialize;
+
+const BASE_URL: &str = "https://hacker-news.firebaseio.com/v0/";
+
+#[derive(Debug, Deserialize)]
+pub struct Story {
+    pub id: u64,
+    pub by: Option<String>,
+    pub title: Option<String>,
+    pub url: Option<String>,
+    pub score: Option<u32>,
+    pub time: Option<u64>,
+    pub descendants: Option<u32>,
+}
+
+pub async fn fetch_top_stories() -> Result<Vec<u64>, Error> {
+    let url = format!("{BASE_URL}topstories.json");
+    let response = reqwest::get(&url).await?;
+    let story_ids: Vec<u64> = response.json().await?;
+    Ok(story_ids)
+}
+
+pub async fn fetch_story_details(story_id: u64) -> Result<Story, Error> {
+    let url = format!("{BASE_URL}item/{story_id}.json");
+    let response = reqwest::get(&url).await?;
+    let story: Story = response.json().await?;
+    Ok(story)
+}
+
+pub async fn fetch_new_stories() -> Result<Vec<u64>, Error> {
+    let url = format!("{BASE_URL}newstories.json");
+    let response = reqwest::get(&url).await?;
+    let story_ids: Vec<u64> = response.json().await?;
+    Ok(story_ids)
+}
+
+pub async fn fetch_ask_stories() -> Result<Vec<u64>, Error> {
+    let url = format!("{BASE_URL}askstories.json");
+    let response = reqwest::get(&url).await?;
+    let story_ids: Vec<u64> = response.json().await?;
+    Ok(story_ids)
+}
+
+pub async fn fetch_show_stories() -> Result<Vec<u64>, Error> {
+    let url = format!("{BASE_URL}showstories.json");
+    let response = reqwest::get(&url).await?;
+    let story_ids: Vec<u64> = response.json().await?;
+    Ok(story_ids)
+}
+
+pub async fn fetch_job_stories() -> Result<Vec<u64>, Error> {
+    let url = format!("{BASE_URL}jobstories.json");
+    let response = reqwest::get(&url).await?;
+    let story_ids: Vec<u64> = response.json().await?;
+    Ok(story_ids)
+}
---
+++ src/main.rs
@@ -0,0 +1,307 @@
+use color_eyre::Result;
+use ratatui::{
+    buffer::Buffer,
+    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    layout::{Constraint, Layout, Rect},
+    style::{
+        palette::tailwind::{BLUE, GREEN, SLATE},
+        Color, Modifier, Style, Stylize,
+    },
+    symbols,
+    text::Line,
+    widgets::{
+        Block, Borders, HighlightSpacing, List, ListItem, ListState, Padding, Paragraph,
+        StatefulWidget, Widget, Wrap,
+    },
+    DefaultTerminal,
+};
+
+mod hnreader;
+
+const HEADER_STYLE: Style = Style::new().fg(SLATE.c100).bg(BLUE.c800);
+const NORMAL_ROW_BG: Color = SLATE.c950;
+const ALT_ROW_BG_COLOR: Color = SLATE.c900;
+const SELECTED_STYLE: Style = Style::new().bg(SLATE.c800).add_modifier(Modifier::BOLD);
+const TEXT_FG_COLOR: Color = SLATE.c200;
+const COMPLETED_TEXT_FG_COLOR: Color = GREEN.c500;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let mut hintapp = App::default();
+
+    match hnreader::fetch_top_stories().await {
+        Ok(story_ids) => {
+            //println!("Top Stories IDs: {:?}", story_ids);
+
+            for sid in story_ids {
+                let mut title = String::from("abc");
+                let mut url = String::from("hcker");
+                match hnreader::fetch_story_details(sid).await {
+                    Ok(story) => {
+                        //println!("Story Details: {:?}", story);
+                        title = story.title.clone().unwrap_or_else(|| String::from("Untitled"));
+                        url = story.url.clone().unwrap_or_else(|| String::from("http://example.com"));
+                    }
+                    Err(err) => eprintln!("Failed to fetch story details: {}", err),
+                }
+                //println!("\n");
+                hintapp.storylist.append_item(HnStoryItem {
+                    story: title,
+                    details: format!("Details From URL: {}", url),
+                    status: Status::Unread,
+                });
+            }
+        }
+        Err(err) => eprintln!("Failed to fetch top stories: {}", err),
+    }
+
+    let res = hintapp.run(terminal);
+    ratatui::restore();
+    res
+}
+
+/// This struct holds the current state of the app. In particular, it has the `list` field
+/// which is a wrapper around `ListState`. Keeping track of the state lets us render the
+/// associated widget with its state and have access to features such as natural scrolling.
+///
+/// Check the event handling at the bottom to see how to change the state on incoming events. Check
+/// the drawing logic for items on how to specify the highlighting style for selected items.
+struct App {
+    should_exit: bool,
+    storylist: HnStoryList,
+}
+
+struct HnStoryList {
+    items: Vec<HnStoryItem>,
+    state: ListState,
+}
+
+#[derive(Debug)]
+struct HnStoryItem {
+    story: String,
+    details: String,
+    status: Status,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum Status {
+    Unread,
+    Read,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            should_exit: false,
+            storylist: HnStoryList::from_iter([
+                (Status::Unread, "Rewrite everything with Rust!", "I can't hold my inner voice. He tells me to rewrite the complete universe with Rust"),
+                (Status::Unread, "Rewrite all of your tui apps with Ratatui", "Yes, you heard that right. Go and replace your tui with Ratatui."),
+                (Status::Unread, "Pet your cat", "Minnak loves to be pet by you! Don't forget to pet and give some treats!"),
+                (Status::Unread, "Walk with your dog", "Max is bored, go walk with him!"),
+                (Status::Unread, "Pay the bills", "Pay the train subscription!!!"),
+                (Status::Unread, "Refactor list example", "If you see this info that means I completed this task!"),
+            ]),
+        }
+    }
+}
+
+impl HnStoryList {
+    fn from_iter<I: IntoIterator<Item = (Status, &'static str, &'static str)>>(iter: I) -> Self {
+        let items = iter
+            .into_iter()
+            .map(|(status, story, details)| HnStoryItem::new(status, story, details))
+            .collect();
+        let state = ListState::default();
+        Self { items, state }
+    }
+
+    fn append_item(&mut self, item: HnStoryItem) {
+        self.items.push(item);
+    }
+}
+
+impl HnStoryItem {
+    fn new(status: Status, story: &str, details: &str) -> Self {
+        Self {
+            status,
+            story: story.to_string(),
+            details: details.to_string(),
+        }
+    }
+}
+
+impl App {
+    fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        while !self.should_exit {
+            terminal.draw(|frame| frame.render_widget(&mut self, frame.area()))?;
+            if let Event::Key(key) = event::read()? {
+                self.handle_key(key);
+            };
+        }
+        Ok(())
+    }
+
+    fn handle_key(&mut self, key: KeyEvent) {
+        if key.kind != KeyEventKind::Press {
+            return;
+        }
+        match key.code {
+            KeyCode::Char('q') | KeyCode::Esc => self.should_exit = true,
+            KeyCode::Char('h') | KeyCode::Left => self.select_none(),
+            KeyCode::Char('j') | KeyCode::Down => self.select_next(),
+            KeyCode::Char('k') | KeyCode::Up => self.select_previous(),
+            KeyCode::Char('g') | KeyCode::Home => self.select_first(),
+            KeyCode::Char('G') | KeyCode::End => self.select_last(),
+            KeyCode::Char('l') | KeyCode::Right | KeyCode::Enter => {
+                self.toggle_status();
+            }
+            _ => {}
+        }
+    }
+
+    fn select_none(&mut self) {
+        self.storylist.state.select(None);
+    }
+
+    fn select_next(&mut self) {
+        self.storylist.state.select_next();
+    }
+    fn select_previous(&mut self) {
+        self.storylist.state.select_previous();
+    }
+
+    fn select_first(&mut self) {
+        self.storylist.state.select_first();
+    }
+
+    fn select_last(&mut self) {
+        self.storylist.state.select_last();
+    }
+
+    /// Changes the status of the selected list item
+    fn toggle_status(&mut self) {
+        if let Some(i) = self.storylist.state.selected() {
+            self.storylist.items[i].status = match self.storylist.items[i].status {
+                Status::Read => Status::Unread,
+                Status::Unread => Status::Read,
+            }
+        }
+    }
+}
+
+impl Widget for &mut App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let [header_area, main_area, footer_area] = Layout::vertical([
+            Constraint::Length(2),
+            Constraint::Fill(1),
+            Constraint::Length(1),
+        ])
+        .areas(area);
+
+        let [list_area, item_area] =
+            Layout::vertical([Constraint::Fill(1), Constraint::Fill(1)]).areas(main_area);
+
+        App::render_header(header_area, buf);
+        App::render_footer(footer_area, buf);
+        self.render_list(list_area, buf);
+        self.render_selected_item(item_area, buf);
+    }
+}
+
+/// Rendering logic for the app
+impl App {
+    fn render_header(area: Rect, buf: &mut Buffer) {
+        Paragraph::new("HackerNews Reader in Terminal using Ratatui")
+            .bold()
+            .centered()
+            .render(area, buf);
+    }
+
+    fn render_footer(area: Rect, buf: &mut Buffer) {
+        Paragraph::new("Use ↓↑ to move, ← to unselect, → to change status, g/G to go top/bottom.")
+            .centered()
+            .render(area, buf);
+    }
+
+    fn render_list(&mut self, area: Rect, buf: &mut Buffer) {
+        let block = Block::new()
+            .title(Line::raw("HackerNews List").centered())
+            .borders(Borders::TOP)
+            .border_set(symbols::border::EMPTY)
+            .border_style(HEADER_STYLE)
+            .bg(NORMAL_ROW_BG);
+
+        // Iterate through all elements in the `items` and stylize them.
+        let items: Vec<ListItem> = self
+            .storylist
+            .items
+            .iter()
+            .enumerate()
+            .map(|(i, storyitem)| {
+                let color = alternate_colors(i);
+                ListItem::from(storyitem).bg(color)
+            })
+            .collect();
+
+        // Create a List from all list items and highlight the currently selected one
+        let list = List::new(items)
+            .block(block)
+            .highlight_style(SELECTED_STYLE)
+            .highlight_symbol(">")
+            .highlight_spacing(HighlightSpacing::Always);
+
+        // We need to disambiguate this trait method as both `Widget` and `StatefulWidget` share the
+        // same method name `render`.
+        StatefulWidget::render(list, area, buf, &mut self.storylist.state);
+    }
+
+    fn render_selected_item(&self, area: Rect, buf: &mut Buffer) {
+        // We get the info depending on the item's state.
+        let info = if let Some(i) = self.storylist.state.selected() {
+            match self.storylist.items[i].status {
+                Status::Read => format!("✓ DONE: {}", self.storylist.items[i].details),
+                Status::Unread => format!("☐ TOREAD: {}", self.storylist.items[i].details),
+            }
+        } else {
+            "Nothing selected...".to_string()
+        };
+
+        // We show the list item's info under the list in this paragraph
+        let block = Block::new()
+            .title(Line::raw("Story Details").centered())
+            .borders(Borders::TOP)
+            .border_set(symbols::border::EMPTY)
+            .border_style(HEADER_STYLE)
+            .bg(NORMAL_ROW_BG)
+            .padding(Padding::horizontal(1));
+
+        // We can now render the item info
+        Paragraph::new(info)
+            .block(block)
+            .fg(TEXT_FG_COLOR)
+            .wrap(Wrap { trim: false })
+            .render(area, buf);
+    }
+}
+
+const fn alternate_colors(i: usize) -> Color {
+    if i % 2 == 0 {
+        NORMAL_ROW_BG
+    } else {
+        ALT_ROW_BG_COLOR
+    }
+}
+
+impl From<&HnStoryItem> for ListItem<'_> {
+    fn from(value: &HnStoryItem) -> Self {
+        let line = match value.status {
+            Status::Unread => Line::styled(format!(" ☐ {}", value.story), TEXT_FG_COLOR),
+            Status::Read => {
+                Line::styled(format!(" ✓ {}", value.story), COMPLETED_TEXT_FG_COLOR)
+            }
+        };
+        ListItem::new(line)
+    }
+}
